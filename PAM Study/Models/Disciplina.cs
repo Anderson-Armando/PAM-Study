752db89c9859ea05859bced0fe2f5ff8f83d46b8
Anderson-Armando <arevalosanderson9@gmail.com>
2024-09-24 12:44:02 -0300

    Implementação dos primeiros componentes e classes

---
+++ PAM Study/Models/Disciplina.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAM_Study.Models
+{
+    public class Disciplina
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ PAM Study/Service/MonitorService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using PAM_Study.Models;
+
+namespace PAM_Study.Service
+{
+    public class MonitorService
+    {
+
+        private HttpClient client;
+        private Models.Monitor monitor;
+        private ObservableCollection<Models.Monitor> monitors;
+        private JsonSerializerOptions serializerOptions;
+        public MonitorService()
+        {
+            client = new HttpClient();
+            serializerOptions = new JsonSerializerOptions {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true
+            };
+        }
+
+        public async Task<List<Models.Monitor>> getAllMonitorsAsync()
+        {
+            Uri uri = new Uri("");
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    monitors = JsonSerializer.Deserialize<ObservableCollection<Models.Monitor>>(content, serializerOptions);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+
+            return monitors;
+        } 
+    } 
+}
